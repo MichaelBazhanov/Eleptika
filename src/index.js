f83dc7eda5ec7681a3f476b11c3429c0e53ae649
michael bazhanov <michaelbazhanov33@gmail.com>
2022-04-05 16:53:19 +0300

    fix slider adopt

--- src/index.js
+++ src/index.js
@@ -4,65 +4,94 @@ import $ from 'jquery' //Если она не нужна то закоммить
 import 'slick-carousel'
 
 $(document).ready(function () {
-  //Инициализация слайдера
-  $('.training-rear-drive').slick({
-    mobileFirst: true,
-    vertical: true,
-    dots: true,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    arrows: false,
-    prevArrow:
-      "<button class='slick-prev slick-arrow' aria-label='Previous' type='button'><svg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'><path stroke-linecap='round' stroke-linejoin='round' d='M15 19l-7-7 7-7'/></svg></button>",
-    nextArrow:
-      "<button class='slick-next slick-arrow' aria-label='Next' type='button'><svg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'><path stroke-linecap='round' stroke-linejoin='round' d='M9 5l7 7-7 7'/></svg></button>",
-    responsive: [
-      {
-        breakpoint: 640,
-        settings: {
-          vertical: false,
-          infinite: true,
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          dots: false,
-          arrows: true,
+  //Инициализация слайдеров
+  $('.training-slider').each(function () {
+    $(this).slick({
+      mobileFirst: true,
+      vertical: true,
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      arrows: false,
+      prevArrow:
+        "<button class='slick-prev slick-arrow' aria-label='Previous' type='button'><svg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'><path stroke-linecap='round' stroke-linejoin='round' d='M15 19l-7-7 7-7'/></svg></button>",
+      nextArrow:
+        "<button class='slick-next slick-arrow' aria-label='Next' type='button'><svg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'><path stroke-linecap='round' stroke-linejoin='round' d='M9 5l7 7-7 7'/></svg></button>",
+      responsive: [
+        {
+          breakpoint: 640,
+          settings: {
+            vertical: false,
+            infinite: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+          },
         },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          vertical: false,
-          infinite: true,
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          dots: false,
-          arrows: true,
+        {
+          breakpoint: 1024,
+          settings: {
+            vertical: false,
+            infinite: true,
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true,
+          },
         },
-      },
-    ],
+      ],
+    })
   })
 
+  // ---------------------------------------------------------------------------------
   // Свернуть и развернуть блок
   let text = ['Свернуть', 'Развернуть']
-  $('.training-drive-button-1').click(function () {
-    let span = $(this).children('span')
-    let t = $(this).children('span').text()
+  $('.training-drive-button-1, .training-drive-button-2').each(function (
+    index,
+    value,
+  ) {
+    $(value).click(function () {
+      //Работа с ребенком
+      let child = '.' + $(this).attr('data-child')
+      let span = $(child)
 
-    if (span.text() === text[0]) {
-      span.text(text[1])
-    } else if (span.text() === text[1]) {
-      span.text(text[0])
-    }
+      if (span.text() === text[0]) {
+        span.text(text[1])
+      } else if (span.text() === text[1]) {
+        span.text(text[0])
+      }
 
-    $('.training-drive-box-1').slideToggle('fast')
+      //работа со слайдером
+      let box = '.' + $(this).attr('data-slider')
+      $(box).slideToggle('fast')
 
-    //Обновление инициализации слайдера
-    setTimeout(function(){
-      $(".training-rear-drive").slick('setPosition');
-    }, 0);
+      //Обновление инициализации слайдера
+      setTimeout(function () {
+        $('.training-slider').slick('setPosition')
+      }, 0)
+    })
   })
+  // ---------------------------------------------------------------------------------
 
+  // $('.training-drive-button-1').click(function () {
+  //   let span = $(this).children('span')
+  //   let t = $(this).children('span').text()
+
+  //   if (span.text() === text[0]) {
+  //     span.text(text[1])
+  //   } else if (span.text() === text[1]) {
+  //     span.text(text[0])
+  //   }
+
+  //   $('.training-drive-box-1').slideToggle('fast')
+
+  //   //Обновление инициализации слайдера
+  //   setTimeout(function () {
+  //     $('.training-slider').slick('setPosition')
+  //   }, 0)
+  // })
 })
 
 // ### #toggle-menu
